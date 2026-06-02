7bb8c8c7b9b800f64d619eee0fe1b82ea5e2b74f
GonzaloGamenara <gonzagamenara@gmail.com>
2026-06-02 01:25:05 -0300

    Tarea 4 completada: desarrollo de la funcion principal de retorno del Controlador de la vista de reportes completada con relaciones Eloquent

--- deheza-challenge/app/Http/Controllers/ReporteController.php
+++ deheza-challenge/app/Http/Controllers/ReporteController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Solicitud;
+//use App\Models\Solicitud; Esto ya no es necesario usando $user->solicitudes() Elocuent ya sabe que modelo instanciar, no necesita importarlo
+
 use Illuminate\Http\Request;
 
 class ReporteController extends Controller
@@ -18,6 +19,19 @@ class ReporteController extends Controller
      */
     public function resumen()
     {
-        return view('reportes.resumen');
+        //Use los mismos nombres de variables de la view del blade para agilizar
+        $user = auth()->user();
+        $total = $user->solicitudes()->count();
+
+        $porEstado = $user->solicitudes()
+            ->selectRaw('estado, count(*) as total')
+            ->groupBy('estado')
+            ->get();
+
+        $ultima = $user->solicitudes()
+            ->latest()
+            ->first();
+
+        return view('reportes.resumen', compact('total', 'porEstado', 'ultima'));
     }
 }
